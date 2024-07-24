1414e2738807e59db87a9f5be73ba39a757e208b
Vishnumurthy Prabhu <vishnumurthyprabhu@Vishnumurthys-MacBook-Pro.local>
2024-07-24 16:08:42 +0530

    fixed ec2_transfer file

--- ec2_transfer.sh
+++ ec2_transfer.sh
@@ -1,3 +1,4 @@
 #bash ec2_transfer.sh
 
-scp -r -i  ~/.ssh/maze-public-ec2.pem /Users/vishnumurthyprabhu/0-1/maze/maze-code/backend/maze-agent-flask/maze_agent_runtime/  ubuntu@13.229.69.52:/home/ubuntu/maze-agent-flask/
+scp -r -i  ~/.ssh/maze-public-ec2.pem /Users/vishnumurthyprabhu/0-1/maze/maze-code/backend/maze-agent-flask/maze_agent_runtime/runtime.py  ubuntu@13.229.69.52:/home/ubuntu/maze-agent-flask/maze_agent_runtime/
+scp -r -i  ~/.ssh/maze-public-ec2.pem /Users/vishnumurthyprabhu/0-1/maze/maze-code/backend/maze-agent-flask/maze_agent_runtime/requirements.txt  ubuntu@13.229.69.52:/home/ubuntu/maze-agent-flask/maze_agent_runtime/
